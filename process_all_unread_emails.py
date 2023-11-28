df191918fb41e0697274fc5eecdcf9a07dcc93e1
peter szemraj <peterszemraj@gmail.com>
2023-11-28 22:40:52 +0100

    🎨 format
    Signed-off-by: peter szemraj <peterszemraj@gmail.com>

--- process_all_unread_emails.py
+++ process_all_unread_emails.py
@@ -1,78 +1,97 @@
 import base64
-from typing import Dict, List, Optional, Union, Tuple
-from googleapiclient.discovery import build, Resource
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from openai import OpenAI
 import os
+from typing import Dict, List, Optional, Tuple, Union
+
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource, build
+from openai import OpenAI
 
 # If modifying these SCOPES, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+
 
 def get_gmail_service():
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    return build('gmail', 'v1', credentials=creds)
+    return build("gmail", "v1", credentials=creds)
+
 
 def get_openai_client():
     # Make sure that OPENAI_API_KEY is set in your environment
     return OpenAI()
+
 
 def get_user_name():
     user_first_name = input("Enter your first name: ")
     user_last_name = input("Enter your last name: ")
     return user_first_name, user_last_name
 
-def fetch_emails(gmail: Resource, page_token: Optional[str]) -> Tuple[List[Dict[str, Union[str, List[str]]]], Optional[str]]:
+
+def fetch_emails(
+    gmail: Resource, page_token: Optional[str]
+) -> Tuple[List[Dict[str, Union[str, List[str]]]], Optional[str]]:
     try:
-        results = gmail.users().messages().list(
-            userId='me',
-            labelIds=['UNREAD'],
-            pageToken=page_token  # Include the page token in the request if there is one
-        ).execute()
+        results = (
+            gmail.users()
+            .messages()
+            .list(
+                userId="me",
+                labelIds=["UNREAD"],
+                pageToken=page_token,  # Include the page token in the request if there is one
+            )
+            .execute()
+        )
     except Exception as e:
         print(f"Failed to fetch emails: {e}")
         return [], None
 
-    messages: List[Dict[str, Union[str, List[str]]]] = results.get('messages', [])
-    page_token = results.get('nextPageToken')
+    messages: List[Dict[str, Union[str, List[str]]]] = results.get("messages", [])
+    page_token = results.get("nextPageToken")
     return messages, page_token
 
-def parse_email_data(gmail: Resource, message_info: Dict[str, Union[str, List[str]]]) -> Dict[str, Union[str, List[str]]]:
+
+def parse_email_data(
+    gmail: Resource, message_info: Dict[str, Union[str, List[str]]]
+) -> Dict[str, Union[str, List[str]]]:
     # Fetch email data with 'full' format
     try:
-        msg = gmail.users().messages().get(
-            userId='me', 
-            id=message_info['id'],
-            format='full'
-        ).execute()
+        msg = (
+            gmail.users()
+            .messages()
+            .get(userId="me", id=message_info["id"], format="full")
+            .execute()
+        )
     except Exception as e:
         print(f"Failed to fetch email data: {e}")
         return {}
 
     try:
-        headers = msg['payload']['headers']
-        subject = next(header['value'] for header in headers if header['name'] == 'Subject')
-        to = next(header['value'] for header in headers if header['name'] == 'To')
-        sender = next(header['value'] for header in headers if header['name'] == 'From')
-        cc = next((header['value'] for header in headers if header['name'] == 'Cc'), None)
+        headers = msg["payload"]["headers"]
+        subject = next(
+            header["value"] for header in headers if header["name"] == "Subject"
+        )
+        to = next(header["value"] for header in headers if header["name"] == "To")
+        sender = next(header["value"] for header in headers if header["name"] == "From")
+        cc = next(
+            (header["value"] for header in headers if header["name"] == "Cc"), None
+        )
     except Exception as e:
         print(f"Failed to parse email data: {e}")
         return {}
@@ -80,27 +99,33 @@ def parse_email_data(gmail: Resource, message_info: Dict[str, Union[str, List[st
     print(f"Fetched email - Subject: {subject}, Sender: {sender}")
 
     # Extract the plain text body
-    parts = msg['payload'].get('parts', [])
+    parts = msg["payload"].get("parts", [])
     for part in parts:
-        if part['mimeType'] == 'text/plain':
-            body = part['body'].get('data', '')
-            body = base64.urlsafe_b64decode(body.encode('ASCII')).decode('utf-8')
+        if part["mimeType"] == "text/plain":
+            body = part["body"].get("data", "")
+            body = base64.urlsafe_b64decode(body.encode("ASCII")).decode("utf-8")
             break
     else:
-        body = ''
+        body = ""
 
     # Parse email data
     email_data_parsed: Dict[str, Union[str, List[str]]] = {
-        'subject': subject,
-        'to': to,
-        'from': sender,
-        'cc': cc,
-        'labels': msg['labelIds'],
-        'body': body,
+        "subject": subject,
+        "to": to,
+        "from": sender,
+        "cc": cc,
+        "labels": msg["labelIds"],
+        "body": body,
     }
     return email_data_parsed
 
-def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name: str, user_last_name: str, client: OpenAI) -> bool:
+
+def evaluate_email(
+    email_data: Dict[str, Union[str, List[str]]],
+    user_first_name: str,
+    user_last_name: str,
+    client: OpenAI,
+) -> bool:
     MAX_EMAIL_LEN = 3000
     system_message: Dict[str, str] = {
         "role": "system",
@@ -112,8 +137,8 @@ def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name
             f"same last name), close acquaintances, or potential contacts {user_first_name} might be interested "
             "in hearing from, are not ignored. You need to distinguish between promotional, automated, "
             "or mass-sent emails and personal communications.\n\n"
-            "Respond with \"True\" if the email is promotional and should be ignored based on "
-            "the below criteria, or \"False\" otherwise. Remember to prioritize personal "
+            'Respond with "True" if the email is promotional and should be ignored based on '
+            'the below criteria, or "False" otherwise. Remember to prioritize personal '
             "communications and ensure emails from genuine individuals are not filtered out.\n\n"
             "Criteria for Ignoring an Email:\n"
             "- The email is promotional: It contains offers, discounts, or is marketing a product "
@@ -132,7 +157,7 @@ def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name
             "such as needing to send a payment via Venmo, but ignore requests for non-essential actions "
             "like purchasing discounted items or signing up for rewards programs.\n\n"
             "Be cautious: If there's any doubt about whether an email is promotional or personal, "
-            "respond with \"False\".\n\n"
+            'respond with "False".\n\n'
             "The user message you will receive will have the following format:\n"
             "Subject: <email subject>\n"
             "To: <to names, to emails>\n"
@@ -141,10 +166,12 @@ def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name
             "Gmail labels: <labels>\n"
             "Body: <plaintext body of the email>\n\n"
             "Your response must be:\n"
-            "\"True\" or \"False\""
-        )
+            '"True" or "False"'
+        ),
     }
-    truncated_body = email_data['body'][:MAX_EMAIL_LEN] + ("..." if len(email_data['body']) > MAX_EMAIL_LEN else "")
+    truncated_body = email_data["body"][:MAX_EMAIL_LEN] + (
+        "..." if len(email_data["body"]) > MAX_EMAIL_LEN else ""
+    )
     user_message: Dict[str, str] = {
         "role": "user",
         "content": (
@@ -154,13 +181,13 @@ def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name
             f"Cc: {email_data['cc']}\n"
             f"Gmail labels: {email_data['labels']}\n"
             f"Body: {truncated_body}"
-        )
+        ),
     }
 
     # Send the messages to GPT-4, TODO add retry logic
     try:
         completion = client.chat.completions.create(
-            model="gpt-4", # switch to gpt-3.5-turbo for faster/ cheaper results (might be slightly less accurate)
+            model="gpt-4",  # switch to gpt-3.5-turbo for faster/ cheaper results (might be slightly less accurate)
             messages=[system_message, user_message],
             max_tokens=1,
             temperature=0.0,
@@ -173,18 +200,21 @@ def evaluate_email(email_data: Dict[str, Union[str, List[str]]], user_first_name
     return completion.choices[0].message.content.strip() == "True"
 
 
-
-
-def process_email(gmail: Resource, message_info: Dict[str, Union[str, List[str]]], email_data_parsed: Dict[str, Union[str, List[str]]], user_first_name: str, user_last_name: str, client: OpenAI) -> int:
+def process_email(
+    gmail: Resource,
+    message_info: Dict[str, Union[str, List[str]]],
+    email_data_parsed: Dict[str, Union[str, List[str]]],
+    user_first_name: str,
+    user_last_name: str,
+    client: OpenAI,
+) -> int:
     # Evaluate email
     if evaluate_email(email_data_parsed, user_first_name, user_last_name, client):
         print("Email is not worth the time, marking as read")
         # Remove UNREAD label
         try:
             gmail.users().messages().modify(
-                userId='me',
-                id=message_info['id'],
-                body={'removeLabelIds': ['UNREAD']}
+                userId="me", id=message_info["id"], body={"removeLabelIds": ["UNREAD"]}
             ).execute()
             print("Email marked as read successfully")
             return 1
@@ -194,11 +224,17 @@ def process_email(gmail: Resource, message_info: Dict[str, Union[str, List[str]]
         print("Email is worth the time, leaving as unread")
     return 0
 
-def report_statistics(total_unread_emails: int, total_pages_fetched: int, total_marked_as_read: int) -> None:
+
+def report_statistics(
+    total_unread_emails: int, total_pages_fetched: int, total_marked_as_read: int
+) -> None:
     print(f"Total number of unread emails fetched: {total_unread_emails}")
     print(f"Total number of pages fetched: {total_pages_fetched}")
     print(f"Total number of emails marked as read: {total_marked_as_read}")
-    print(f"Final number of unread emails: {total_unread_emails - total_marked_as_read}")
+    print(
+        f"Final number of unread emails: {total_unread_emails - total_marked_as_read}"
+    )
+
 
 def main():
     gmail = get_gmail_service()
@@ -218,17 +254,27 @@ def main():
         print(f"Fetched page {total_pages_fetched} of emails")
 
         total_unread_emails += len(messages)
-        for message_info in messages: # TODO process emails on a single page in parallel
+        for (
+            message_info
+        ) in messages:  # TODO process emails on a single page in parallel
             # Fetch and parse email data
             email_data_parsed = parse_email_data(gmail, message_info)
 
             # Process email
-            total_marked_as_read += process_email(gmail, message_info, email_data_parsed, user_first_name, user_last_name, client)
+            total_marked_as_read += process_email(
+                gmail,
+                message_info,
+                email_data_parsed,
+                user_first_name,
+                user_last_name,
+                client,
+            )
 
         if not page_token:
             break  # Exit the loop if there are no more pages of messages
 
     report_statistics(total_unread_emails, total_pages_fetched, total_marked_as_read)
+
 
 if __name__ == "__main__":
     main()
